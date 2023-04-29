a2015ab32e4ecb042455b04ebf41d5f40f3fe53a
alfonsoLeandro <leandroalfonsoporley@gmail.com>
2023-04-29 00:03:45 -0300

    Agrega conexion a DB y lectura de archivos a Main

--- src/main/java/com/github/alfonsoleandro/Main.java
+++ src/main/java/com/github/alfonsoleandro/Main.java
@@ -1,11 +1,60 @@
 package com.github.alfonsoleandro;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author alfonsoLeandro
  * @since 1.0.0
  */
 public class Main {
+
+    private final Connection connection;
+
+    public Main() throws ClassNotFoundException, SQLException, FileNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        this.connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/pastabase",
+                "root",
+                "bernardo");
+
+        loadData();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            new Main();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadData() throws FileNotFoundException {
+        File archivos = new File("src/main/resources/archivos");
+        for (File f : archivos.listFiles()) {
+            if (f.isDirectory()) continue;
+            System.out.println(f.getName());
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(f));
+            //Orden de tablas:
+            // 1- circuits
+            // 2- constructor
+            // 3- driver
+            // 4- race
+            // 5- constructor_results
+            // 6- qualifying
+            // 7- pit_stops
+            // 8- results
+            // 9- status
+            // 10- seasons
+            // 11- lap_times
+            // 12- driver_standings
+            // 13- constructor_standings
+            // 14- sprint results
+        }
     }
 }
