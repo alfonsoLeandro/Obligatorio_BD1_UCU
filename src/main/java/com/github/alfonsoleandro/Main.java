03c3123af7ddb0c3a0d0ae14eec03c352a4ee198
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2023-07-03 00:22:19 -0300

    Adds loadFile method for loading a file using reflection to call the corresponding insert method.

--- src/main/java/com/github/alfonsoleandro/Main.java
+++ src/main/java/com/github/alfonsoleandro/Main.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.*;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author alfonsoLeandro, Joelit0
@@ -33,25 +35,67 @@ public class Main {
                 "root",
                 "bernardo");
 
+        boolean tablesExisted = checkIfTablesExist();
+
         // Creates all the necessary tables, read from the "tables.sql" file.
         executeFile(new File("src/main/resources/sql/create_tables.sql"));
 
-        // Alters all the necessary tables, read from the "alters.sql" file.
-        executeFile(new File("src/main/resources/sql/alter_tables.sql"));
+        if (!tablesExisted) {
+            // Alters all the necessary tables, read from the "alters.sql" file.
+            executeFile(new File("src/main/resources/sql/alter_tables.sql"));
+            loadData();
+        }
 
 
-        loadData();
     }
+
+    /**
+     * Checks if any table of interest exists in the database.
+     */
+    private boolean checkIfTablesExist() throws SQLException {
+        boolean tablesExist = false;
+        DatabaseMetaData metaData = this.connection.getMetaData();
+        ResultSet resultSet = metaData.getTables(null, BD_NAME, "%", null);
+        List<String> tableNames = Arrays.asList("circuits",
+                "constructor_results",
+                "constructor_standings",
+                "constructors",
+                "driver_standings",
+                "drivers",
+                "lap_times",
+                "pit_stops",
+                "qualifyings",
+                "races",
+                "results",
+                "seasons",
+                "sprint_results",
+                "status");
+
+        while (resultSet.next()) {
+            String tableName = resultSet.getString("TABLE_NAME");
+            if (tableNames.contains(tableName)) {
+                System.out.println(tableName);
+                System.out.println("existia la tabla \"" + tableName + "\", los alters (foreign keys) no se ejecutarán.");
+                tablesExist = true;
+                break;
+            }
+        }
+
+        resultSet.close();
+        return tablesExist;
+    }
+
 
     /**
      * Executes a list of statements in a given file.
      */
     private void executeFile(File inputFile) throws SQLException, FileNotFoundException {
+        System.out.println("Ejecutando archivo \"" + inputFile.getName() + "\"...");
         BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
         StringBuilder sb = new StringBuilder();
         bufferedReader.lines().forEach(sb::append);
         for (String statement : sb.toString().split(";")) {
-            this.connection.prepareStatement(statement+";").execute();
+            this.connection.prepareStatement(statement + ";").execute();
         }
     }
 
@@ -59,10 +103,46 @@ public class Main {
      * Loads the data from the csv files to the database.
      */
     private void loadData() throws FileNotFoundException {
-        File archivo = new File("src/main/resources/archivos/prueba.csv");
+        for (String fileName : new String[]{
+                "circuits",
+                "constructor_results",
+                "constructor_standings",
+                "constructors",
+                "driver_standings",
+                "drivers",
+                "lap_times",
+                "pit_stops",
+                "qualifying",
+                "races",
+                "results",
+                "seasons",
+                "sprint_results",
+                "status"}) {
+            System.out.println("Inserting data into \"" + fileName + ".csv\"...");
+            loadFile(fileName);
+        }
+
+    }
+
+    /**
+     * Loads a file and inserts its data to the database.
+     */
+    private void loadFile(String fileName) throws FileNotFoundException {
+        File archivo = new File("src/main/resources/archivos/"+fileName+".csv");
         BufferedReader bufferedReader = new BufferedReader(new FileReader(archivo));
-        bufferedReader.lines().forEach( line -> {
-            String[] datos = line.split(",");
+        bufferedReader.lines().skip(1).forEach(line -> {
+            String insertQuery;
+            try {
+                Method method = SqlHelper.class.getMethod(fileName, List.class);
+                insertQuery = (String) method.invoke(null, Arrays.asList(line.split(",")));
+            } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                this.connection.prepareStatement(insertQuery).execute();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         });
 
 
