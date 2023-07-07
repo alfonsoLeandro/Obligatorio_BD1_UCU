01ce2b0893c93630e3f8341b1053ec2adb8c38ff
alfonsoLeandro <leandroalfonsoporley@gmail.com>
2023-07-06 21:42:46 -0300

    Agrega query #2 a QueryHelper

--- src/main/java/com/github/alfonsoleandro/QueryHelper.java
+++ src/main/java/com/github/alfonsoleandro/QueryHelper.java
@@ -6,4 +6,23 @@ package com.github.alfonsoleandro;
  * @author alfonsoLeandro
  */
 public class QueryHelper {
+
+    /**
+     * Query #2: Get constructor with most wins
+     */
+    public static String mostWinsInConstructor() {
+        return "WITH RECURSIVE race_maxp AS (SELECT race_id, MAX(points) AS nax_points FROM constructor_results GROUP BY race_id) " +
+                "SELECT c.name, COUNT(*) as wins " +
+                "FROM race_maxp rmp " +
+                "         JOIN constructor_results cr ON cr.race_id = rmp.race_id " +
+                "         JOIN constructors c ON cr.constructor_id = c.constructor_id " +
+                "WHERE cr.points = rmp.nax_points " +
+                "  AND cr.constructor_id = (SELECT MIN(constructor_id) " +
+                "                           FROM constructor_results " +
+                "                           WHERE race_id = rmp.race_id " +
+                "                             AND points = rmp.nax_points) " +
+                "GROUP BY c.name " +
+                "ORDER BY wins DESC " +
+                "LIMIT 1;";
+    }
 }
