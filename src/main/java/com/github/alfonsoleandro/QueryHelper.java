f04cabe5c98ada44f825fd86dfda5331d8638a1e
alfonsoLeandro <leandroalfonsoporley@gmail.com>
2023-07-07 10:59:37 -0300

    Cambia a bloque de string multilinea. Agrega query N4

--- src/main/java/com/github/alfonsoleandro/QueryHelper.java
+++ src/main/java/com/github/alfonsoleandro/QueryHelper.java
@@ -11,18 +11,31 @@ public class QueryHelper {
      * Query #2: Get constructor with most wins
      */
     public static String mostWinsInConstructor() {
-        return "WITH RECURSIVE race_maxp AS (SELECT race_id, MAX(points) AS nax_points FROM constructor_results GROUP BY race_id) " +
-                "SELECT c.name, COUNT(*) as wins " +
-                "FROM race_maxp rmp " +
-                "         JOIN constructor_results cr ON cr.race_id = rmp.race_id " +
-                "         JOIN constructors c ON cr.constructor_id = c.constructor_id " +
-                "WHERE cr.points = rmp.nax_points " +
-                "  AND cr.constructor_id = (SELECT MIN(constructor_id) " +
-                "                           FROM constructor_results " +
-                "                           WHERE race_id = rmp.race_id " +
-                "                             AND points = rmp.nax_points) " +
-                "GROUP BY c.name " +
-                "ORDER BY wins DESC " +
-                "LIMIT 1;";
+        return """
+                WITH RECURSIVE race_maxp AS (SELECT race_id, MAX(points) AS nax_points FROM constructor_results GROUP BY race_id)
+                SELECT c.name, COUNT(*) as wins
+                FROM race_maxp rmp
+                         JOIN constructor_results cr ON cr.race_id = rmp.race_id
+                         JOIN constructors c ON cr.constructor_id = c.constructor_id
+                WHERE cr.points = rmp.nax_points
+                  AND cr.constructor_id = (SELECT MIN(constructor_id)
+                                           FROM constructor_results
+                                           WHERE race_id = rmp.race_id
+                                             AND points = rmp.nax_points)
+                GROUP BY c.name
+                ORDER BY wins DESC
+                LIMIT 1;
+                """;
+    }
+
+    /**
+     * Query #4: Get big races between  1996 and 1999
+     */
+    public static String bigRacesBetweenYears() {
+        return """
+                SELECT distinct(name)
+                FROM races
+                WHERE year BETWEEN 1996 AND 1999;
+                """;
     }
 }
